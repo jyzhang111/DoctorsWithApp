ec9cfa5918b25516259f364bf38315621d3eeeb4
ypark20 <47282177+ypark20@users.noreply.github.com>
2019-04-21 00:34:55 -0400

    Update NotificationHelper.java

--- DoctorsWithApp/app/src/main/java/cis350/upenn/edu/doctorswithapp/NotificationHelper.java
+++ DoctorsWithApp/app/src/main/java/cis350/upenn/edu/doctorswithapp/NotificationHelper.java
@@ -9,11 +9,24 @@ import android.content.ContextWrapper;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
+
+import java.util.Map;
+import java.util.TreeSet;
+
+import cis350.upenn.edu.doctorswithapp.shared_classes.MedicationInfo;
+
 public class NotificationHelper extends ContextWrapper {
     public static final String channelL1ID = "channel1ID";
     public static final String channelL1Name = "Channel 1";
 
     private NotificationManager mManager;
+    boolean isFirstTime = true;
+
+
+    private Map<String, TreeSet<MedicationInfo>> medications = MainActivity.processor.getMedications();
+
+
+
 
     public NotificationHelper(Context base) {
         super(base);
@@ -42,8 +55,46 @@ public class NotificationHelper extends ContextWrapper {
     }
 
     public NotificationCompat.Builder getChannel1Notification(String title, String message) {
-        return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
-                .setContentText(message).setSmallIcon(R.drawable.ic_one);
-    }
+        if (isFirstTime) {
+            isFirstTime = false;
+            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+                    .setContentText(message).setSmallIcon(R.drawable.ic_red);
 
+        } else {
+            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+                    .setContentText(message).setSmallIcon(R.drawable.ic_blue);
+        }
+//        String color = message.split(":")[0];
+//        if (color.equals("red")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_red);
+//        } else if (color.equals("orange")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_orange);
+//        } else if (color.equals("yellow")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_yellow);
+//        } else if (color.equals("green")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_green);
+//        } else if (color.equals("blue")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_blue);
+//        } else if (color.equals("indigo")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_indigo);
+//        } else if( color.equals("violet")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_violet);
+//        } else if (color.equals("brown")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_brown);
+//        } else if (color.equals("white")) {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_white);
+//        } else {
+//            return new NotificationCompat.Builder(getApplicationContext(), channelL1ID).setContentTitle(title)
+//                    .setContentText(message).setSmallIcon(R.drawable.ic_black);
+//        }
+    }
 }
