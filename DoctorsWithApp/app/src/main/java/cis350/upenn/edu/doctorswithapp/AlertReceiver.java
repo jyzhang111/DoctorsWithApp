e15e3be0c76978083009620d9a7b4f32da12a00a
ypark20 <47282177+ypark20@users.noreply.github.com>
2019-05-04 21:02:56 -0400

    Update AlertReceiver.java

--- DoctorsWithApp/app/src/main/java/cis350/upenn/edu/doctorswithapp/AlertReceiver.java
+++ DoctorsWithApp/app/src/main/java/cis350/upenn/edu/doctorswithapp/AlertReceiver.java
@@ -5,12 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+import java.util.Map;
+import java.util.TreeSet;
+
+import cis350.upenn.edu.doctorswithapp.shared_classes.MedicationInfo;
+
 public class AlertReceiver extends BroadcastReceiver {
+    private Map<String, TreeSet<MedicationInfo>> medications = MainActivity.processor.getMedications();
+
     @Override
     public void onReceive(Context context, Intent intent) {
+        TreeSet<MedicationInfo> medicationSet = medications.get(HomePageActivity.patientName);
         NotificationHelper notificationHelper = new NotificationHelper(context);
         NotificationCompat.Builder nb = notificationHelper.getChannel1Notification("Pill Reminder",
-                "Take your pills now!");
+                "Take ");
         notificationHelper.getManager().notify(1, nb.build());
     }
 }
